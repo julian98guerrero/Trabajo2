d6424aa9c35a0cdc0b0dbe163129113302c19d82
julian98guerrero <julianaguerrero@ucompensar.edu.co>
2022-08-30 21:26:04 -0500

    commit init

---
+++ Trabajo2/Trabajo2/Entidades/Entity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3.Entidades
+{
+    internal class Entity
+    {
+        public string Id { get; private set; }
+
+        public string Placa { get; private set; } 
+        public Entity(string id, string placa)
+        {
+            Id = id;
+            Placa = placa;
+        }
+
+ 
+    }
+}
---
+++ Trabajo2/Trabajo2/Program.cs
@@ -0,0 +1,12 @@
+﻿using ConsoleApp3.Entidades;
+
+
+//polimorfismo y caste (atribuos)
+Entity ve = new VehiculoElectrico(id: "1", placa: "hgb233", horasCarga: 50.1);
+var ve1 = (VehiculoElectrico)ve;
+Console.WriteLine(ve1.HorasCarga);
+
+//Polimorfismo y caste (metodo)
+Entity vg = new VehiculoGasolina(id: "2", placa: "hnn322");
+var vg1 = (VehiculoGasolina)vg;
+vg1.ImprimirValores();
